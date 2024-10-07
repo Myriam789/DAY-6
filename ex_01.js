5cbc1e0d924839b96dad4f11c132b0fe4969ec10
Myriam789 <myriam.sall@epitech.digital>
2024-10-07 11:58:38 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,12 @@
+function displayAgentInfo(agent){
+    const infoDiv = document.getElementById("agent-info")
+    const paragraph = document.createElement('p')
+
+   paragraph.textContent = "Nom:" + ",Age:" + ",first name:" + ",last name"
+
+   infoDiv.appendChild(paragraph);
+}
+
+const agent = {
+    age : 
+}
